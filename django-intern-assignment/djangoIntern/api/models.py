a4a087f1d42637bf8ad68c2ab39fbdb9c5ddf64d
Shivkumar Verma <113050680+KurmiShiv@users.noreply.github.com>
2023-05-03 11:12:52 +0530

    models.py

--- django-intern-assignment/djangoIntern/api/models.py
+++ django-intern-assignment/djangoIntern/api/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+from django.contrib import admin
+from .models import Artist, Work
+
 class Client(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -27,4 +30,27 @@ class Work(models.Model):
 class Artist(models.Model):
     name = models.CharField(max_length=255)
     work = models.ManyToManyField(Work)
+    def __str__(self):
+        return self.name
+        
+# add some dummy data
+
+
+admin.site.register(Artist)
+admin.site.register(Work)
+
+# Add some dummy data
+artist1 = Artist.objects.create(name='John Doe')
+artist2 = Artist.objects.create(name='Jane Smith')
+
+work1 = Work.objects.create(link='https://www.youtube.com/watch?v=dQw4w9WgXcQ', work_type='youtube')
+work1.artist.add(artist1)
+
+work2 = Work.objects.create(link='https://www.instagram.com/p/CQwfn3Dj4zg/', work_type='instagram')
+work2.artist.add(artist2)
+
+work3 = Work.objects.create(link='https://www.youtube.com/watch?v=ZVGfV5eG4xo', work_type='youtube')
+work3.artist.add(artist1, artist2)
+
+
 
